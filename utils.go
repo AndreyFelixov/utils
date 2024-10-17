61376ca72a70b82fd8852ac5406be68458a7c0f3
AndreyFelixov <andreyokay1337@gmail.com>
2024-10-17 16:36:58 +0300

    Add ContaintsInt

--- utils.go
+++ utils.go
@@ -6,5 +6,16 @@ func Contains(a []string, x string) bool {
 			return true
 		}
 	}
+
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+
 	return false
 }
